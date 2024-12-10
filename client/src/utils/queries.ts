5b3646a60af3de8ac43fc1458d4117a7376445ae
melanieprimpas <melanie.primpas@gmail.com>
2024-12-10 16:43:36 -0500

    login function now works, still testing and debugging save book

--- client/src/utils/queries.ts
+++ client/src/utils/queries.ts
@@ -4,10 +4,10 @@ import { gql } from '@apollo/client';
 // Use the gql function to access the thoughts entrypoint and export it
 export const GET_ME = gql`
   query me {
-    _id: ID!
-    username: String!
-    email: String!
-    savedBooks: [Book]
-    bookCount: Int
+    _id
+    username
+    email
+    savedBooks
+    bookCount
   }
 `;
